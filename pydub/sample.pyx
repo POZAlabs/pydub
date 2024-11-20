cf60dec5b2376a59722d226e44fea69b7b16e013
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-11-20 13:43:58 +0900

    IP-5910: [pydub] 24bit 오디오 부호 확장 연산 개선 (#24)

--- pydub/sample.pyx
+++ pydub/sample.pyx
@@ -1,35 +1,35 @@
-import array
-
 cimport cython
 from libc.stdlib cimport free, malloc
+from libc.string cimport memcpy, memset
+
+
+DEF BYTES_PER_24BIT_SAMPLE = 3
+DEF BYTES_PER_32BIT_SAMPLE = 4
 
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def convert_24bit_to_32bit(const unsigned char[:] data):
-    cdef int i
-    cdef int p = 0
-    cdef int len_data = data.size
-    cdef int len_result = len_data // 3 * 4
-    cdef unsigned char b0, b1, b2
-    cdef unsigned char *result_bytes = <unsigned char *> malloc(len_result * sizeof(unsigned char))
+@cython.cdivision(True)
+def extend_24bit_to_32bit(const unsigned char[:] data):
+    cdef:
+        int input_size = data.size
+        int output_size = input_size // BYTES_PER_24BIT_SAMPLE * BYTES_PER_32BIT_SAMPLE
+        int num_samples = input_size // BYTES_PER_24BIT_SAMPLE
+        int sample_idx = 0
+        unsigned char* input_ptr = <unsigned char*>&data[0]
+        unsigned char* output_ptr = <unsigned char*> malloc(output_size * sizeof(unsigned char))
 
-    if result_bytes == NULL:
+    if output_ptr == NULL:
         raise MemoryError("Could not allocate memory for result array")
 
     try:
-        for i in range(0, len_data, 3):
-            b0 = data[i]
-            b1 = data[i + 1]
-            b2 = data[i + 2]
+        for sample_idx in range(num_samples):
+            # Extend sign bit
+            output_ptr[sample_idx * BYTES_PER_32BIT_SAMPLE] = (input_ptr[2] >> 7) * 0xff
+            # Copy last 3 bytes from source
+            memcpy(output_ptr + (sample_idx * BYTES_PER_32BIT_SAMPLE) + 1, input_ptr, BYTES_PER_24BIT_SAMPLE)
+            input_ptr += BYTES_PER_24BIT_SAMPLE
 
-            result_bytes[p] = 0xFF if b2 > 0x7f else 0x00
-            result_bytes[p + 1] = b0
-            result_bytes[p + 2] = b1
-            result_bytes[p + 3] = b2
-
-            p += 4
-
-        return bytes(result_bytes[:len_result])
+        return bytes(output_ptr[:output_size])
     finally:
-        free(result_bytes)
+        free(output_ptr)

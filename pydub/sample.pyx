19dce5d8fd8d005f8a24e3c6d01ce9381eb38e93
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2025-01-13 19:04:35 +0900

    d: 미사용 참조 및 주석 제거

--- pydub/sample.pyx
+++ pydub/sample.pyx
@@ -1,6 +1,5 @@
 cimport cython
 from cpython.bytes cimport PyBytes_FromStringAndSize, PyBytes_AS_STRING
-from libc.string cimport memcpy, memset
 
 
 DEF BYTES_PER_24BIT_SAMPLE = 3
@@ -10,9 +9,9 @@ DEF BYTES_PER_32BIT_SAMPLE = 4
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-def extend_24bit_to_32bit(const unsigned char[::1] data):  # Use contiguous memory view
+def extend_24bit_to_32bit(const unsigned char[::1] data):
     cdef:
-        int input_size = data.shape[0]  # More efficient than data.size
+        int input_size = data.shape[0]
         int output_size
         int num_samples
         int sample_idx = 0
@@ -21,23 +20,18 @@ def extend_24bit_to_32bit(const unsigned char[::1] data):  # Use contiguous memo
         object output_bytes
         unsigned char sign_bit
 
-    # Validate input size
     if input_size % BYTES_PER_24BIT_SAMPLE:
         raise ValueError("Input size must be a multiple of 3 bytes")
 
-    # Pre-calculate sizes once
     num_samples = input_size // BYTES_PER_24BIT_SAMPLE
     output_size = num_samples * BYTES_PER_32BIT_SAMPLE
 
-    # Get input pointer directly from contiguous memory view
     input_ptr = &data[0]
 
-    # Create output bytes with exact size needed
     output_bytes = PyBytes_FromStringAndSize(NULL, output_size)
     if not output_bytes:
         raise MemoryError("Could not allocate memory for output")
 
-    # Get direct pointer to output buffer
     output_buffer = <unsigned char*>PyBytes_AS_STRING(output_bytes)
 
     for sample_idx in range(num_samples):
